5629d4e53070f4d621723931e5f529aaecd1b029
wang7211401 <wang7211401@163.com>
2017-09-19 20:12:46 +0800

    登录有个bug

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 
 import Vue from 'vue'
+import AV from 'leancloud-storage'
+
+var APP_ID = 'nkiykFU7nT78x35oWE3HCRkP-gzGzoHsz';
+var APP_KEY = 'XQcFGxVQXa3kHMQXVnK1mpt9';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
 var app = new Vue({
   el: '#app',
   data: {
+    actionType:'signUp',
+    formData:{
+      username:'',
+      password:''
+    },
     newTodo:'',
-    todoList:[]
+    todoList:[],
+    currentUser:null
   },
   created:function(){
     window.onbeforeunload =()=>{
@@ -14,6 +29,7 @@ var app = new Vue({
     let oldDataString = window.localStorage.getItem('myTodos')
     let oldData = JSON.parse(oldDataString)
     this.todoList = oldData || []
+    this.currentUser = this.getCurrentUser()
   },
   methods:{
     addTodo:function(){
@@ -27,6 +43,39 @@ var app = new Vue({
     removeTodo:function(todo){
       let index = this.todoList.indexOf(todo)
       this.todoList.splice(index,1)
+    },
+    signUp:function(){
+      var user = new AV.User();
+      // 设置用户名
+      user.setUsername(this.formData.username);
+      // 设置密码
+      user.setPassword(this.formData.password);
+      user.signUp().then((loginedUser)=> {
+        this.currentUser = this.getCurrentUser()
+      },(error)=>{
+        alert('注册失败')
+      });
+    },
+    login:function(){
+      AV.User.logIn(this.formData.username, this.formData.password).then((loginedUser)=> {
+        this.currentUser = this.getCurrentUser()
+      },(error)=>{
+        alert('登录失败')
+      });
+    },
+    getCurrentUser:function(){
+      let current = AV.User.current()
+      if(current){
+        let {id,createdAt,attributes:{username}} = current
+        return {id,username,createdAt}
+      }else{
+        return null
+      }
+    },
+    logout:function(){
+      AV.User.logOut();
+      this.currentUser = null
+      window.location.reload()
     }
   }
 })
